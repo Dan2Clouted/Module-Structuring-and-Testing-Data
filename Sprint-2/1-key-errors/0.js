0224852edbbe2364f8c02322186822507bb22909
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2026-02-11 14:11:13 +0000

    key errors completed

--- Sprint-2/1-key-errors/0.js
+++ Sprint-2/1-key-errors/0.js
@@ -1,13 +1,26 @@
 // Predict and explain first...
 //  =============> write your prediction here
 
+// The code will throw an error because the variable "str" is being declared twice in the same scope.
+
 // call the function capitalise with a string input
 // interpret the error message and figure out why an error is occurring
 
-function capitalise(str) {
-  let str = `${str[0].toUpperCase()}${str.slice(1)}`;
-  return str;
-}
+//Original code with errors:
+
+// function capitalise(str) {
+//   let str = `${str[0].toUpperCase()}${str.slice(1)}`;
+//   return str;
+// }
 
 // =============> write your explanation here
+// The error occurs because the variable "str" is being declared twice in the same scope. This causes a conflict and results in a syntax error.
+// To fix this error, we can simply remove the "let" keyword when reassigning the value to "str" inside the function. This way we are not declaring a new variable, but rather reassigning the existing parameter "str".
+
 // =============> write your new code here
+// Updated Code:
+function capitalise(str) {
+  str = `${str[0].toUpperCase()}${str.slice(1)}`;
+  return str;
+}
+console.log(capitalise("hello"));
