5729ef62ae4e1c3823aa50b8d0ba9bc3b98045d6
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2026-02-12 13:06:26 +0000

    key errors completed

--- Sprint-2/1-key-errors/1.js
+++ Sprint-2/1-key-errors/1.js
@@ -3,18 +3,32 @@
 // Why will an error occur when this program runs?
 // =============> write your prediction here
 
+// The program will throw an error because decimalNumber is declared twice in the same function scope. And also decimalNumber is used in console.log(decimalNumber) outside the function, where it doesn't exist.
+
 // Try playing computer with the example to work out what is going on
 
+// Code with errors:
+
+// function convertToPercentage(decimalNumber) {
+// const decimalNumber = 0.5
+//const percentage = `${decimalNumber * 100}%`;
+//
+// return percentage;
+//}
+// console.log((decimalNumber));
+
+// =============> write your explanation here
+
+// The error occurs because the variable "decimalNumber" is being declared twice in the same scope. This causes a conflict and results in a syntax error. Additionally, "decimalNumber" is being used in console.log(decimalNumber) outside the function, where it doesn't exist, which will also cause a ReferenceError.
+
+// To fix this error, i simply removed the second declaration of decimalNumber inside the function and also changed console.log(decimalNumber) to console.log(convertToPercentage(0.5)) to call the function with an argument and log the result.
+
+// Finally, correct the code to fix the problem
+// =============> write your new code here
+
 function convertToPercentage(decimalNumber) {
-  const decimalNumber = 0.5;
   const percentage = `${decimalNumber * 100}%`;
 
   return percentage;
 }
-
-console.log(decimalNumber);
-
-// =============> write your explanation here
-
-// Finally, correct the code to fix the problem
-// =============> write your new code here
+console.log(convertToPercentage(0.5)); // output: 50%
--- Sprint-2/1-key-errors/2.js
+++ Sprint-2/1-key-errors/2.js
@@ -1,20 +1,27 @@
-
 // Predict and explain first BEFORE you run any code...
+// The code will throw an error because of an unexpected number "3" in the function parameter. The function parameter must be a variable name, not a number.
+// Also, there is no num parameter declared in the function, so even if the function definition worked, it would sill cause a reference error when trying to use "num" in the return statement.
 
 // this function should square any number but instead we're going to get an error
 
 // =============> write your prediction of the error here
 
-function square(3) {
-    return num * num;
-}
+//function square(3) {
+//    return num * num;
+//}
 
 // =============> write the error message here
+// SyntaxError: Unexpected number in function parameter list. This error occurs because "3" is not a valid parameter name. Parameters should be variable names that can be used within the function body.
+//ReferenceError: num is not defined. This error occurs because "num" is being used in the return statement, but it has not been declared or defined anywhere in the function.
 
 // =============> explain this error message here
+// To fix this error, we need to change the function parameter from "3" to a valid variable name, such as "num". This way, we can pass a number as an argument when calling the function, and it will be used correctly within the function body to calculate the square of that number.
 
 // Finally, correct the code to fix the problem
 
 // =============> write your new code here
 
-
+function square(num) {
+  return num * num;
+}
+console.log(square(3)); // output: 9
