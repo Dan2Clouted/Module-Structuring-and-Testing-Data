442dbc8777dbe98e388b97ef4c26774f649b52c0
Daniel Aderibigbe <danieladeribigbe07@gmail.com>
2026-02-18 23:35:11 +0000

    test update

--- Sprint-2/2-mandatory-debug/0.js
+++ Sprint-2/2-mandatory-debug/0.js
@@ -1,14 +1,22 @@
 // Predict and explain first...
 
 // =============> write your prediction here
+// The code will throw an error message because multiply only logged the result and didnt return it. So when we try to use the result of multiply(10, 32) in the template literal, it will be undefined, which will cause an error when trying to perform string interpolation with an undefined value.
 
-function multiply(a, b) {
-  console.log(a * b);
-}
+// function multiply(a, b) {
+//  console.log(a * b);
+//}
 
-console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
+//console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`);
 
 // =============> write your explanation here
+// To fix this error, we need to change the multiply function to return the result of the multiplication instead of just logging it. This way, when we call multiply(10, 32) inside the template literal, it will return the correct value (320) that can be used in the string interpolation without causing an error.
 
 // Finally, correct the code to fix the problem
 //  =============> write your new code here
+
+function multiply(a, b) {
+  return a * b;
+}
+
+console.log(`The result of multiplying 10 and 32 is ${multiply(10, 32)}`); // output: The result of multiplying 10 and 32 is 320
--- Sprint-2/2-mandatory-debug/1.js
+++ Sprint-2/2-mandatory-debug/1.js
@@ -1,13 +1,22 @@
 // Predict and explain first...
 //  =============> write your prediction here
+// The code will throw an error message because the function "sum" has a return statement that is not returning any value.
 
+//function sum(a, b) {
+//  return;
+//  a + b;
+//}
+
+//console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
+
+// =============> write your explanation here
+// The function "sum" is not returning any value.
+// To fix the error, we need to change the return statement to return the result of the addition of "a" and "b".
+
+// Finally, correct the code to fix the problem
+//  =============> write your new code here
 function sum(a, b) {
-  return;
-  a + b;
+  return a + b;
 }
 
 console.log(`The sum of 10 and 32 is ${sum(10, 32)}`);
-
-// =============> write your explanation here
-// Finally, correct the code to fix the problem
-//  =============> write your new code here
